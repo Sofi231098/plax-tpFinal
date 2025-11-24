3aada2f985df5d684ffdd3941772d54fe76cf057
aleyufra <alejandromaryufra@gmail.com>
2025-11-24 02:16:12 -0300

    eliminando archivos innecesarios

--- 01-setup.sql
+++
@@ -1,8 +0,0 @@
-CREATE USER 'clusteradmin'@'%' IDENTIFIED BY 'cladmin';
-GRANT ALL privileges ON *.* TO 'clusteradmin'@'%' with grant option;
-RESET MASTER;
-
-CREATE DATABASE IF NOT EXISTS plax_db;
-CREATE USER IF NOT EXISTS 'plax'@'%' IDENTIFIED BY 'plax';
-GRANT ALL PRIVILEGES ON plax_db.* TO 'plax'@'%';
-FLUSH PRIVILEGES;
